8d0fd7be282150cafe621c4c4a0e48932b4799ed
m1r24n <m.irzan@gmail.com>
2026-03-01 15:51:44 +0700

    update

--- Lab/switching_lab/svr_config.sh
+++ Lab/switching_lab/svr_config.sh
@@ -1,7 +1,7 @@
 # node SVR
 #!/bin/bash
 VM=svr1
-MAC=56:04:15:00:6b:40
+MAC=56:04:15:00:54:96
 IPv4=192.168.10.101/24
 IPv6=fc00:dead:beef:a10::1000:101/64
 GWv4=192.168.10.1
@@ -36,19 +36,43 @@ EOF
 sudo netplan apply
 
 
+# sw1
 
-set interfaces ge-0/0/0 description "connection to port eth1 of svr1"
+set chassis aggregated-devices ethernet device-count 8
+delete  interfaces ge-0/0/0 mtu
 set interfaces ge-0/0/0 gigether-options 802.3ad ae0
-set interfaces ge-0/0/1 description "connection to port eth2 of svr1"
+delete  interfaces ge-0/0/1 mtu
 set interfaces ge-0/0/1 gigether-options 802.3ad ae0
-set interfaces ge-0/0/2 description "connection to port eth1 of svr2"
-set interfaces ge-0/0/2 mtu 9000
-set interfaces ge-0/0/3 description "connection to port eth2 of svr2"
-set interfaces ge-0/0/3 mtu 9000
+set interfaces ae0 mtu 9014
 set interfaces ae0 aggregated-ether-options lacp active
-set interfaces ae0 unit 0 family ethernet-switching interface-mode access
-set interfaces ae0 unit 0 family ethernet-switching vlan members vlan10
-set interfaces fxp0 unit 0 family inet address 172.16.11.10/24
-set interfaces irb unit 10 family inet address 192.168.10.1/24
-set vlans vlan10 vlan-id 10
-set vlans vlan10 l3-interface irb.10
+set interfaces ae0 unit 0 family inet address 10.0.0.0/31
+set interfaces ae0 unit 0 family inet6
+set interfaces fxp0 unit 0 family inet address 172.16.11.1/24
+set interfaces lo0 unit 0 family inet address 10.0.255.1/32
+set protocols ospf area 0.0.0.0 interface lo0.0 passive
+set protocols ospf area 0.0.0.0 interface ae0.0 interface-type p2p
+
+
+# sw2
+interface lag 1
+    no shutdown
+    ip mtu 9000
+    ip address 10.0.0.1/31
+    lacp mode active
+    ip ospf 1 area 0.0.0.0
+    ip ospf network point-to-point
+interface 1/1/1
+    no shutdown
+    mtu 9000
+    lag 1
+interface 1/1/2
+    no shutdown
+    mtu 9000
+    lag 1
+interface loopback 0
+    ip address 10.0.255.2/32
+    ip ospf 1 area 0.0.0.0
+!
+router ospf 1
+    router-id 10.0.255.2
+    area 0.0.0.0
